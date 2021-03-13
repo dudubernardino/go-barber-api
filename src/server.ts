af4af3f1b1d00559dc65e46f0c36cbcd415166f0
Eduardo <eduardomoura.moura@gmail.com>
2021-03-13 08:33:14 -0300

    :rocket: Initial Commit -> TS config

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,7 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World!'});
-})
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
