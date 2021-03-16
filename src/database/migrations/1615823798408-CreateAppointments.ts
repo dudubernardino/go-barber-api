7b2f0de5b1f37970236c6c9b9dfb94b1be9dff25
Eduardo <eduardomoura.moura@gmail.com>
2021-03-16 19:37:14 -0300

    :rocket: User -> Model && Routes && Services

--- src/database/migrations/1615823798408-CreateAppointments.ts
+++ src/database/migrations/1615823798408-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1615823798408 implements MigrationInterfa
           columns: [
             {
               name: 'id',
-              type: 'varchar',
+              type: 'uuid',
               isPrimary: true,
               generationStrategy: 'uuid',
               default: 'uuid_generate_v4()',
@@ -17,13 +17,21 @@ export default class CreateAppointments1615823798408 implements MigrationInterfa
             {
               name: 'provider',
               type: 'varchar',
-              isNullable: false
             },
             {
               name: 'date',
               type: 'timestamp with time zone',
-              isNullable: false
-            }
+            },
+            {
+              name: 'created_at',
+              type: 'timestamp',
+              default: 'now()'
+            },
+            {
+              name: 'updated_at',
+              type: 'timestamp',
+              default: 'now()'
+            },
           ]
         })
       )
