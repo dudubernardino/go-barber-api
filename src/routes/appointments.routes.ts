65bb769344d57cc2ab1a541e1f1dbd2261f5ef0b
Eduardo <eduardomoura.moura@gmail.com>
2021-03-19 10:15:02 -0300

    :rocket: Exception Handling

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -16,22 +16,18 @@ appointmentsRouter.get('/', async (request, response) => {
   const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
-})
+});
 
 appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
+  const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
+  const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService();
 
-    const appointment = await createAppointment.execute({ date: parsedDate, provider_id });
+  const appointment = await createAppointment.execute({ date: parsedDate, provider_id });
 
   return response.json(appointment);
-  } catch(err) {
-    return response.status(400).json({ error: err.message });
-  }
 });
 
 export default appointmentsRouter;
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,6 +1,8 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+
 import Appointment from '../models/Appointment';
 import AppointmentRepository from '../repositories/AppointmentsRepository';
 
@@ -18,7 +20,7 @@ class CreateAppointmentService {
     const findAppointmentInSameData =  await appointmentsRepository.findByDate(appointmentDate);
 
     if (findAppointmentInSameData) {
-      throw Error('This appointment is already booked');
+      throw new AppError('This appointment is already booked');
     }
 
     const appointment = appointmentsRepository.create({
